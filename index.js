3d8f532ec812eee50bcf396f1ec80613756e28dc
Your Name <you@example.com>
2019-02-16 13:45:38 +0900

    2019.02.16

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 // index.js
-
-const validate = v=>o=>Object.entries(v).reduce(
-    (prev, [k, fs])=>prev.concat([
-        {[k]: fs.reduce((p, f)=>p.concat(f(o[k])), [])}
-    ]), []
-)
+const validate = v=>{
+    const validates = {}
+    v({
+        add: (k, f, s=true)=>
+            validates[k] ? validates[k].push({fn:f, skip:s}) 
+	                 : validates[k] = [{fn:f, skip:s}]
+    })
+    return o=>Object.entries(validates).reduce(
+        (prev, [k, fs])=>prev.concat([{
+            [k]: fs.reduce(
+                (p, {fn, skip})=>(p.includes(false) && skip) ? p.concat(null) : p.concat(fn(o[k]))
+               ,[]
+	    )
+	}])
+       ,[]
+    )
+}
 
 module.exports = validate
 
@@ -16,25 +27,19 @@ const toStr = Object.prototype.toString
 const o = {
     id: 12345
    ,name: 'foo bar'
-   ,birthday : new Date()
+   ,birthday : new Date(2019, 2-1, 16)
    ,sex: false
 }
-const v = {
-    id: [
-        v=>toStr.call(v)==='[object Number]'
-       ,v=>v>=0
-       ,v=>v<=20000
-    ]
-   ,name: [
-        v=>toStr.call(v)==='[object String]'
-       ,v=>v.length>=1 && v.length<=10
-       ,v=>v.match("\\w+\\s+\\w+") ? true : false
-    ]
-   ,birthday : [
-        v=>toStr.call(v)==='[object Date]'
-    ]
-   ,sex: [
-        v=>toStr.call(v)==='[object Boolean]'
-    ]
+const v = f=>{
+    f.add('id', v=>toStr.call(v)==='[object Number]', false)
+    f.add('id', v=>v>=0)
+    f.add('id', v=>v<=20000)
+    f.add('name', v=>toStr.call(v)==='[object String]', false)
+    f.add('name', v=>v.length>=1 && v.length<=10)
+    f.add('name', v=>v.match("\\w+\\s+\\w+") ? true : false)
+    f.add('birthday', v=>toStr.call(v)==='[object Date]', false)
+    f.add('birthday', v=>v>=new Date(1900, 1-1, 1))
+    f.add('birthday', v=>v<new Date(3000, 12-1, 31))
+    f.add('sex', v=>toStr.call(v)==='[object Boolean]', false)
 }
 console.log(validate(v)(o))

814b2f34694fb3a90c3d5984b83c015dd34c4806
Your Name <you@example.com>
2019-02-24 21:39:50 +0900

    2019.02.24

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 // index.js
-const validate = v=>o=>Object.entries(v).reduce(
-    (p0, [k0, v0])=>{
-        p0[k0] = Object.entries(v0).reduce(
-            (p1, [k1, {fn, skip}])=>{
-                Object.values(p1).includes(false) && skip ?
-                    p1[k1] = null : p1[k1] = fn(o[k0])
-                return p1
-	    }, {}
-	)
-        return p0
-    }, {}
-)
+const validate = f=>{
+    const m = {}
+    f({
+        add: (id, key, fn)=>{
+            m[id] ? m[id].push({key:key, fn:fn}) : m[id] = [{key:key, fn:fn}]
+	}
+    })
+    return o=>Object.entries(m).reduce(
+        (p0, [id, arr])=>{
+            p0[id] = arr.reduce(
+                (p1, {key, fn})=>{
+                    Object.values(p1).includes(false) ?
+                        p1[key] = null : p1[key] = fn(o[id]) 
+                    return p1
+		}, {}
+	    )
+            return p0
+	}, {}
+    )
+}
 
 module.exports = validate
 
@@ -26,22 +34,16 @@ const o = {
 }
 
 const toStr = Object.prototype.toString
-const res = require('./index.js')({
-    id: {
-        type: {fn: v=>toStr.call(v)==='[object Number]', skip: false}
-       ,min:  {fn: v=>v>=0, skip: true}
-       ,max:  {fn: v=>v<20000, skip: true}
-    }
-   ,name : {
-        type: {fn: v=>toStr.call(v)==='[object String]', skip: false}
-       ,range:{fn: v=>v.length>=1 && v.length<=10, skip: true}
-       ,fmt:  {fn: v=>v.match("\\S+\\s+\\S+") ? true : false, skip: true}
-    }
-   ,birthday: {
-        type: {fn: v=>toStr.call(v)==='[object Date]', skip: false}
-       ,min:  {fn: v=>v>=new Date(1900, 1-1, 1), skip: true}
-       ,max:  {fn: v=>v<new Date(3000, 12-1, 31), skip: true}
-    }
+const res = require('./index.js')(({add})=>{
+    add('id', 'type', v=>toStr.call(v)==='[object Number]') 
+    add('id', 'min', v=>v>=0)
+    add('id', 'max', v=>v<20000)
+    add('name', 'type', v=>toStr.call(v)==='[object String]')
+    add('name', 'range', v=>v.length>=1 && v.length<=10)
+    add('name', 'fmt', v=>v.match(/\S+\s+\S+/) ? true : false)
+    add('birthday', 'type', v=>toStr.call(v)==='[object Date]')
+    add('birthday', 'min', v=>v>=new Date(1900, 1-1, 1))
+    add('birthday', 'max', v=>v<new Date(3000, 12-1, 31))
 })(o)
 
 console.log(res)
